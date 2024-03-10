d845743777a1540e00c1dc682335044cb89ce1f2
vajra <kowtilyavajra@gmail.com>
2024-03-10 12:10:08 +0530

    First Commit

---
+++ new.js
@@ -0,0 +1,14 @@
+// function showSubcategories(category) {
+//     if (category === "music") {
+//       document.getElementById("subcategory").innerHTML = `
+//         <option value="">Select a subcategory</option>
+//         <option value="rock">Rock</option>
+//         <option value="pop">Pop</option>
+//         <option value="jazz">Jazz</option>
+//       `;
+//     } else {
+//       document.getElementById("subcategory").innerHTML = `
+//         <option value="">Select a subcategory</option>
+//       `;
+//     }
+//   }
